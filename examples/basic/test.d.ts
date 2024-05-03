532295cf4872506d56c3585332f930e6962e5b71
zach <zach@dylibso.com>
2024-05-03 15:50:07 -0700

    refactor: add reason argument to assert import (#5)

--- examples/basic/test.d.ts
+++ examples/basic/test.d.ts
@@ -4,7 +4,7 @@ declare module "main" {
 
 declare module "xtp:test" {
   interface harness {
-    assert(name: PTR, value: I64);
+    assert(name: PTR, value: I64, reason: PTR);
     call(func: PTR, input: PTR): PTR;
     time(func: PTR, input: PTR): I64;
     group(name: PTR);
